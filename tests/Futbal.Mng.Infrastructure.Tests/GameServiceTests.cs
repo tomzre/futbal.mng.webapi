3e64d63d54f5b44d4a237eea5d6130147342edb2
tomzre <tomzre@pop-os.localdomain>
2019-12-27 18:03:54 +0100

    test fix

--- tests/Futbal.Mng.Infrastructure.Tests/GameServiceTests.cs
+++ tests/Futbal.Mng.Infrastructure.Tests/GameServiceTests.cs
@@ -34,20 +34,21 @@ namespace Tests
         [Test]
         public async Task GetUserGames_ShouldReturn_AllGamesThatUserBePartOf()
         {
+            //this test doesnt make any sense :) just here for no reason.
             //Arrange
-            var sut = new GameService(_gameRepoMock.Object, _userRepoMock.Object, _mapper.Object);
-            _mapper.Setup(m => m.Map<IList<GameDetailsDto>>(It.IsAny<object>()))
-                .Returns(new List<GameDetailsDto>{
-                    new GameDetailsDto()
-                });
             _gameRepoMock.Setup(m => m.GetUserGames(It.IsAny<Guid>()))
                 .ReturnsAsync(new List<Game>());
+                
+            _mapper.Setup(m => m.Map<IList<GameDetailsGridDto>>(It.IsAny<object>()))
+                .Returns(new List<GameDetailsGridDto>());
+            
+            var sut = new GameService(_gameRepoMock.Object, _userRepoMock.Object, _mapper.Object);
 
             //Act
             var result = await sut.GetUserGames(It.IsAny<Guid>());
             
             //Assert
-            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(0, result.Count());
         }
     }
 }
