c72f73204bfc5107208ed758fead3258d7ab2c18
tomzre <palmask8@gmail.com>
2020-01-04 23:41:59 +0100

    refactored usegames endpoint

--- src/Futbal.Mng.Infrastructure/QueryHandler/GetAllUserGamesQuery.cs
+++ src/Futbal.Mng.Infrastructure/QueryHandler/GetAllUserGamesQuery.cs
@@ -5,19 +5,28 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Futbal.Mng.Domain.Event;
 using Futbal.Mng.Domain.UserManagement;
-using Futbal.Mng.Domain.ValueObjects;
 using Futbal.Mng.Infrastructure.DTO;
-using Futbal.Mng.Infrastructure.Interfaces;
+using Futbal.Mng.Infrastructure.Interfaces.QueryHandler;
 
-namespace Futbal.Mng.Infrastructure.GameManagement
+namespace Futbal.Mng.Infrastructure.QueryHandler
 {
-    public class GameService : IGameService
+    public class GetAllUserGamesQuery: IQuery<IList<UserGamesListDto>>
+    {
+        public Guid UserId { get; }
+
+        public GetAllUserGamesQuery(Guid userId)
+        {
+            UserId = userId;
+        }
+    }
+
+    internal class GetAllUserGamesHandler : IHandleQuery<GetAllUserGamesQuery, IList<UserGamesListDto>>
     {
         private readonly IGameRepository _gameRepository;
         private readonly IMapper _mapper;
         private readonly IUserRepository _userRepository;
 
-        public GameService(IGameRepository gameRepository,
+        public GetAllUserGamesHandler(IGameRepository gameRepository,
         IUserRepository userRepository,
         IMapper mapper)
         {
@@ -25,15 +34,15 @@ namespace Futbal.Mng.Infrastructure.GameManagement
             _mapper = mapper;
             _gameRepository = gameRepository;
         }
-        
-        public async Task<IEnumerable<GameDetailsGridDto>> GetUserGames(Guid userId)
+
+        public async Task<IList<UserGamesListDto>> HandleAsync(GetAllUserGamesQuery query)
         {
-            var userGames = await _gameRepository.GetUserGames(userId);
+            var userGames = await _gameRepository.GetUserGames(query.UserId);
 
             if(userGames == null)
-                return new List<GameDetailsGridDto>();
+                return new List<UserGamesListDto>();
 
-            var mappedGames = _mapper.Map<IList<GameDetailsGridDto>>(userGames);
+            var mappedGames = _mapper.Map<IList<UserGamesListDto>>(userGames);
 
             foreach (var mappedGame in mappedGames)
             {
