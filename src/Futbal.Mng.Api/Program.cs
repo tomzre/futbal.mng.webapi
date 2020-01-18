894ddb8d0953baf14f4166c59568250a000f843f
Tomasz Repel <tomaszrepel@pop-os.localdomain>
2020-01-18 16:07:45 +0100

    configuration moved to program

--- src/Futbal.Mng.Api/Program.cs
+++ src/Futbal.Mng.Api/Program.cs
@@ -20,10 +20,18 @@ namespace Futbal.Mng.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options => {
+                .UseKestrel(options =>
+                {
                     options.Listen(IPAddress.Any, 5000); //HTTP port
-                    //options.Listen(IPAddress.Loopback, 6010, cfg => cfg.UseHttps()); //HTTPS port
-                    })
+                                                         //options.Listen(IPAddress.Loopback, 6010, cfg => cfg.UseHttps()); //HTTPS port
+                })
+                .ConfigureAppConfiguration((WebHostBuilderContext ctx, IConfigurationBuilder builder) =>
+                {
+                    builder.SetBasePath(ctx.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("./appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"./appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: false)
+                    .AddEnvironmentVariables();
+                })
                 .UseStartup<Startup>();
     }
 }
